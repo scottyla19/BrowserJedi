4fec5a8d09ff6a925f1fb6ebec8cf9823d7c2b54
Scott LaForest <scottyla19@gmail.com>
2015-04-26 13:20:42 -0600

    added python and arduino files

---
+++ README.md
@@ -0,0 +1,10 @@
+#Browser Jedi
+
+##Control your browser with a Hover Board [Hover Labs](http://www.hoverlabs.co), an Arduino, and some python.
+
+Currently it only works on a Mac and was tested on Firefox, Crome, and Safari.
+
+Things to do in the future:
+- ensure cross-browser functionality
+- develop a cross-platform version (Windows and Linux)
+- allow user specified key commands instead of hard coding
---
+++ serial-comm.py
@@ -0,0 +1,53 @@
+import serial #Import Serial Library
+import os
+
+
+
+arduinoSerialData = serial.Serial('/dev/cu.usbmodem411',9600) #Create Serial port object called arduinoSerialData
+def press_key_command(data):
+	print data
+	action = ''
+	if "=" in data:
+		action = data.split("=")[1].strip()
+
+	print action
+	actions = {"Left Swipe":"(ASCII character 8)",
+				"Up Swipe":['"+"', "{command down}"],#get correct key code
+				"Right Swipe":["(ASCII character 8)","{shift down}"],
+				"Down Swipe":['"-"',"{command down}"],#get correct key code
+				"Tap West":['"z"',"{command down}"],
+				"Tap East":['"z"',"{command down, shift down}"],
+				"Tap North":["space","{shift down}"],
+				"Tap South":"space",
+				"Tap Center":['"r"',"{command down, shift down}"]}
+	if action in actions:
+		key_press = actions[action]
+		
+		cmd = ""
+		if action == "Tap South" or action == "Left Swipe":
+			cmd = """
+			osascript -e 'tell application "System Events" to keystroke {} '
+			""".format(key_press)
+		
+
+		else:
+			cmd = """
+			osascript -e 'tell application "System Events" to keystroke {} using {}'
+			""".format(*key_press)
+
+		print cmd
+		os.system(cmd)
+		
+	else:
+		pass
+	
+
+ 
+
+while (1==1):
+    if (arduinoSerialData.inWaiting()>0):
+        myData = arduinoSerialData.readline()
+        press_key_command(myData)
+        
+
+
